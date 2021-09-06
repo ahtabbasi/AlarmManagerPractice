64338f2b658a35bf7828bbb6a56f736ca3d1bc0e
Ahtsham Abbasi <ahtabbasi@gmail.com>
2021-09-06 13:58:43 +0200

    Request ignore battery optimization inside the app

--- app/src/main/java/com/ahtabbasi/alarmmanagerpractice/MainActivity.kt
+++ app/src/main/java/com/ahtabbasi/alarmmanagerpractice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ahtabbasi.alarmmanagerpractice
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.PowerManager
 import android.provider.Settings
@@ -24,7 +25,8 @@ class MainActivity : AppCompatActivity() {
         val pm = getSystemService(POWER_SERVICE) as PowerManager
         if (!pm.isIgnoringBatteryOptimizations(packageName)) {
             showOptimizationRationale {
-                intent.action = Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS
+                intent.action = Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
+                intent.data = Uri.parse("package:$packageName")
                 startActivity(intent)
             }
         }
